615da7fbab420a7ab123337de875d3fd7625220a
Snufkin0866 <snufkin0866@gmail.com>
2020-09-28 21:27:43 +0900

    エラーコード追加など

--- python_bitbankcc/private_api.py
+++ python_bitbankcc/private_api.py
@@ -2,19 +2,18 @@
 # -*- coding: utf-8 -*-
 #
 # MIT License
-# 
+
 # Copyright (c) 2017 bitbank, inc. (ビットバンク株式会社)
-# 
+
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -42,6 +41,7 @@ def sign_request(key, query):
     h = hmac.new(bytearray(key, 'utf8'), bytearray(query, 'utf8'), sha256)
     return h.hexdigest()
 
+
 def make_header(query_data, api_key, api_secret):
     nonce = str(int(time.time() * 1000))
     message = nonce + query_data
@@ -52,13 +52,14 @@ def make_header(query_data, api_key, api_secret):
         'ACCESS-SIGNATURE': sign_request(api_secret, message)
     }
 
+
 class bitbankcc_private(object):
-    
+
     def __init__(self, api_key, api_secret):
         self.end_point = 'https://api.bitbank.cc/v1'
         self.api_key = api_key
         self.api_secret = api_secret
-    
+
     def _get_query(self, path, query):
         data = '/v1' + path + urlencode(query)
         logger.debug('GET: ' + data)
@@ -66,7 +67,7 @@ class bitbankcc_private(object):
         uri = self.end_point + path + urlencode(query)
         response = requests.get(uri, headers=headers)
         return error_parser(response.json())
-    
+
     def _post_query(self, path, query):
         data = json.dumps(query)
         logger.debug('POST: ' + data)
@@ -74,16 +75,16 @@ class bitbankcc_private(object):
         uri = self.end_point + path
         response = requests.post(uri, data=data, headers=headers)
         return error_parser(response.json())
-    
+
     def get_asset(self):
         return self._get_query('/user/assets', {})
-    
+
     def get_order(self, pair, order_id):
         return self._get_query('/user/spot/order?', {
             'pair': pair,
             'order_id': order_id
         })
-    
+
     def get_active_orders(self, pair, options=None):
         if options is None:
             options = {}
@@ -99,7 +100,7 @@ class bitbankcc_private(object):
             'side': side,
             'type': order_type
         })
-    
+
     def cancel_order(self, pair, order_id):
         return self._post_query('/user/spot/cancel_order', {
             'pair': pair,
@@ -118,11 +119,9 @@ class bitbankcc_private(object):
             'order_ids': order_ids
         })
 
-    def get_trade_history(self, pair, order_count):
+    def get_trade_history(self, pair, **params):
         return self._get_query('/user/spot/trade_history?', {
-            'pair': pair,
-            'count': order_count
-        })
+            **{'pair': pair}, **params})
 
     def get_withdraw_account(self, asset):
         return self._get_query('/user/withdrawal_account?', {
